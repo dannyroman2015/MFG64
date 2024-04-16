4aa902c7112a601333fbb462cb795f698331c0a5
dannyroman2015 <iknowihave@gmail.com>
2024-04-17 06:13:22 +0700

    1704

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "github.com/lib/pq"
+	"github.com/xuri/excelize/v2"
 )
 
 // func process(w http.ResponseWriter, r *http.Request) {
@@ -10,8 +11,12 @@ import (
 // }
 
 func main() {
+	f, _ := excelize.OpenFile("firsttest.xlsx")
+	f.SetCellFormula("Sheet2", "A1", "=SUM(A2:A3)")
 
-	dbstore := NewPosgreDB()
-	Server := NewServer(":3000", dbstore.db)
-	Server.Run()
+	f.Save()
+
+	// dbstore := NewPosgreDB()
+	// Server := NewServer(":3000", dbstore.db)
+	// Server.Run()
 }
