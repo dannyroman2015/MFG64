712f50f2e10b39e40804936a1e84e94f2d8115f2
dannyroman2015 <iknowihave@gmail.com>
2024-04-16 17:58:20 +0700

    16041801

--- main.go
+++ main.go
@@ -1,37 +1,34 @@
 package main
 
 import (
-	"fmt"
-
 	_ "github.com/lib/pq"
-	"github.com/xuri/excelize/v2"
 )
 
 func main() {
-	f, err := excelize.OpenFile("firsttest.xlsx")
-	if err != nil {
-		fmt.Println(err)
-	}
-	defer func() {
-		if err := f.Close(); err != nil {
-			fmt.Println(err)
-		}
-	}()
+	// f, err := excelize.OpenFile("firsttest.xlsx")
+	// if err != nil {
+	// 	fmt.Println(err)
+	// }
+	// defer func() {
+	// 	if err := f.Close(); err != nil {
+	// 		fmt.Println(err)
+	// 	}
+	// }()
 
-	cell, err := f.GetCellValue("Sheet2", "A2")
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(cell)
+	// cell, err := f.GetCellValue("Sheet2", "A2")
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(cell)
 
-	rows, _ := f.GetRows("Sheet2")
-	for _, v := range rows {
-		for _, c := range v {
-			fmt.Println(c)
-		}
-	}
+	// rows, _ := f.GetRows("Sheet2")
+	// for _, v := range rows {
+	// 	for _, c := range v {
+	// 		fmt.Println(c)
+	// 	}
+	// }
 
-	// dbstore := NewPosgreDB()
-	// Server := NewServer(":3000", dbstore.db)
-	// Server.Run()
+	dbstore := NewPosgreDB()
+	Server := NewServer(":3000", dbstore.db)
+	Server.Run()
 }
