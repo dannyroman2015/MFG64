134bf6d7ff52742c51cbc1fb49e37ee487de05b6
dannyroman2015 <iknowihave@gmail.com>
2024-06-02 12:59:02 +0700

    sua loi tiep tmp_target

--- api.go
+++ api.go
@@ -907,23 +907,24 @@ func (s *Server) efficientChartHandler(c *fiber.Ctx) error {
 		var a, b string
 		var c, d float64
 		rows.Scan(&a, &b, &c, &d)
-		// i := slices.Index(datesOfTarget, a)
+		i := slices.Index(datesOfTarget, a)
 		a = strings.Split(a, "T")[0]
 		t, _ := time.Parse("2006-01-02", a)
 		a = t.Format("2 Jan")
-
-		if d == 0 {
+		if d == 0 || i == -1 {
 			efficiency = append(efficiency, 0)
 		} else {
-			// efficiency = append(efficiency, math.Round((c/d)*100/tmp_targets[i]))
-			efficiency = append(efficiency, math.Round((c/d)*100))
+			efficiency = append(efficiency, math.Round((c/d)*100/tmp_targets[i]))
 		}
 
 		labels = append(labels, a)
 		c = math.Round(c)
 		quanity = append(quanity, c)
-		// targets = append(targets, tmp_targets[i]*d)
-		targets = append(targets, d)
+		if i == -1 {
+			targets = append(targets, 0)
+		} else {
+			targets = append(targets, tmp_targets[i]*d)
+		}
 	}
 
 	var latestCreated string
