34f8ba4efc8a8d7f22314590ed2890e8bb1d9aff
dannyroman2015 <iknowihave@gmail.com>
2024-06-03 14:30:15 +0700

    them so luong vao xem efficienct

--- handlers.go
+++ handlers.go
@@ -901,12 +901,13 @@ func (s *Server) viewreportPostHandler(c *fiber.Ctx) error {
 	f.SetCellValue("Sheet1", "F1", "To")
 	f.SetCellValue("Sheet1", "G1", todate)
 	f.SetCellValue("Sheet1", "A2", "Ngày giờ nhập")
-	f.SetCellValue("Sheet1", "B2", "Số Lượng")
+	f.SetCellValue("Sheet1", "B2", "Sản Lượng")
 	f.SetCellValue("Sheet1", "C2", "Manhr")
 	f.SetCellValue("Sheet1", "D2", "Xưởng")
 	f.SetCellValue("Sheet1", "E2", "Loại Hàng")
+	f.SetCellValue("Sheet1", "F2", "Số lượng (pcs)")
 	f.SetActiveSheet(1)
-	sql := `select created_datetime, qty, manhr, factory_no, type from efficienct_reports 
+	sql := `select created_datetime, qty, manhr, factory_no, type, pcs from efficienct_reports 
 		where work_center ='` + workcenter + `' and date >='` + fromdate + `' and date <='` + todate + `' order by date desc, created_datetime desc`
 
 	rows, err := s.db.Query(sql)
@@ -917,9 +918,9 @@ func (s *Server) viewreportPostHandler(c *fiber.Ctx) error {
 	var data [][]string
 	i := 3
 	for rows.Next() {
-		var a = make([]string, 5)
+		var a = make([]string, 6)
 		var t string
-		rows.Scan(&t, &a[1], &a[2], &a[3], &a[4])
+		rows.Scan(&t, &a[1], &a[2], &a[3], &a[4], &a[5])
 
 		a[0] = t[0:19]
 		a[0] = strings.Replace(a[0], "T", " ", 1)
@@ -930,6 +931,7 @@ func (s *Server) viewreportPostHandler(c *fiber.Ctx) error {
 		f.SetCellValue("Sheet1", fmt.Sprintf("C%d", i), a[2])
 		f.SetCellValue("Sheet1", fmt.Sprintf("D%d", i), a[3])
 		f.SetCellValue("Sheet1", fmt.Sprintf("E%d", i), a[4])
+		f.SetCellValue("Sheet1", fmt.Sprintf("F%d", i), a[5])
 		i++
 	}
 	if err := f.SaveAs("./static/uploads/Book1.xlsx"); err != nil {
